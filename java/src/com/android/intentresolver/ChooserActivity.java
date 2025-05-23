39156d2c3b686d19cf592928fda991a70f8a78d7
Andrey Yepin <ayepin@google.com>
2025-05-22 23:09:24 -0700

    Sanitize cross-profile intents.
    Remove package or component information from payload intents (and their
    selectors) for cross-profile sharing.
    
    Bug: 407764858
    Test: manual testing with test app
    Flag: EXEMPT bugfix
    (cherry picked from https://googleplex-android-review.googlesource.com/q/commit:b4fde3a7c886d253ca3028aa685f021d046a1d60)
    Merged-In: Ie00f0a4e2fdb0cb7a3b2a148369d435d7da9b1bc
    Change-Id: Ie00f0a4e2fdb0cb7a3b2a148369d435d7da9b1bc

--- java/src/com/android/intentresolver/ChooserActivity.java
+++ java/src/com/android/intentresolver/ChooserActivity.java
@@ -26,6 +26,7 @@ import static android.stats.devicepolicy.nano.DevicePolicyEnums.RESOLVER_EMPTY_S
 
 import static androidx.lifecycle.LifecycleKt.getCoroutineScope;
 
+import static com.android.intentresolver.util.IntentUtils.sanitizePayloadIntents;
 import static com.android.internal.util.LatencyTracker.ACTION_LOAD_SHARE_SHEET;
 
 import android.app.Activity;
@@ -521,9 +522,12 @@ public class ChooserActivity extends Hilt_ChooserActivity implements
             boolean filterLastUsed,
             TargetDataLoader targetDataLoader) {
         int selectedProfile = findSelectedProfile();
+        List<Intent> crossProfileIntents = sanitizePayloadIntents(mIntents);
         ChooserGridAdapter personalAdapter = createChooserGridAdapter(
                 /* context */ this,
-                /* payloadIntents */ mIntents,
+                /* payloadIntents */ selectedProfile == PROFILE_PERSONAL
+                        ? mIntents
+                        : crossProfileIntents,
                 selectedProfile == PROFILE_PERSONAL ? initialIntents : null,
                 rList,
                 filterLastUsed,
@@ -531,7 +535,9 @@ public class ChooserActivity extends Hilt_ChooserActivity implements
                 targetDataLoader);
         ChooserGridAdapter workAdapter = createChooserGridAdapter(
                 /* context */ this,
-                /* payloadIntents */ mIntents,
+                /* payloadIntents */ selectedProfile == PROFILE_WORK
+                        ? mIntents
+                        : crossProfileIntents,
                 selectedProfile == PROFILE_WORK ? initialIntents : null,
                 rList,
                 filterLastUsed,
