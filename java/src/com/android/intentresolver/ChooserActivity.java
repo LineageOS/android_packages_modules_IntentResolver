c8764cf58dacbe30adafe53eb34612e0cad23717
Matt Casey <mrcasey@google.com>
2025-06-17 19:30:30 +0000

    Launch image editor as sharesheet-launching user (flag off)
    ag/34038177 but for the cases where use_preferred_image_editor is off or
    does not exist.
    
    Bug: 407991863
    Test: Manual repro of bug and verification of fix.
    Test: atest IntentResolver-tests-*
    Flag: EXEMPT bugfix
    (cherry picked from commit 29da3d663edd566f0b5c4dce434f40aa7e3877de)
    (cherry picked from https://googleplex-android-review.googlesource.com/q/commit:f54a0bbd15ffe6bf3c90f362c18487de4fbbc5ac)
    Merged-In: Idf2d76615f4396f09e7de33becc87afaea782d41
    Change-Id: Idf2d76615f4396f09e7de33becc87afaea782d41

--- java/src/com/android/intentresolver/ChooserActivity.java
+++ java/src/com/android/intentresolver/ChooserActivity.java
@@ -2236,10 +2236,10 @@ public class ChooserActivity extends Hilt_ChooserActivity implements
                 this::getFirstVisibleImgPreviewView,
                 new ChooserActionFactory.ActionActivityStarter() {
                     @Override
-                    public void safelyStartActivityAsPersonalProfileUser(TargetInfo targetInfo) {
+                    public void safelyStartActivityAsLaunchingUser(TargetInfo targetInfo) {
                         safelyStartActivityAsUser(
                                 targetInfo,
-                                mProfiles.getPersonalHandle()
+                                mUserInteractor.getLaunchedAs()
                         );
                         Log.d(TAG, "safelyStartActivityAsPersonalProfileUser("
                                 + targetInfo + "): finishing!");
@@ -2247,13 +2247,13 @@ public class ChooserActivity extends Hilt_ChooserActivity implements
                     }
 
                     @Override
-                    public void safelyStartActivityAsPersonalProfileUserWithSharedElementTransition(
+                    public void safelyStartActivityAsLaunchingUserWithSharedElementTransition(
                             TargetInfo targetInfo, View sharedElement, String sharedElementName) {
                         ActivityOptions options = ActivityOptions.makeSceneTransitionAnimation(
                                 ChooserActivity.this, sharedElement, sharedElementName);
                         safelyStartActivityAsUser(
                                 targetInfo,
-                                mProfiles.getPersonalHandle(),
+                                mUserInteractor.getLaunchedAs(),
                                 options.toBundle());
                         // Can't finish right away because the shared element transition may not
                         // be ready to start.
