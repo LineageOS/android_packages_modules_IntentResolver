552e8f6cbb6042a279838d6b62b93e8a1e959330
Andrey Yepin <ayepin@google.com>
2026-02-06 15:26:09 -0800

    Sanitize alternate intents.
    Exclude ineligible alternate intents from cross-profile target
    resolution.
    
    Bug: 444443479
    Test: manual testing with a test app
    Test: new STS tests
    Flag: EXEMPT CVE_FIX
    Change-Id: I85ece5179da3f4908870be96174be4ec4fd912e5

--- java/src/com/android/intentresolver/AbstractMultiProfilePagerAdapter.java
+++ java/src/com/android/intentresolver/AbstractMultiProfilePagerAdapter.java
@@ -15,6 +15,8 @@
  */
 package com.android.intentresolver;
 
+import static com.android.intentresolver.util.IntentUtils.isCrossProfileIntent;
+
 import android.annotation.IntDef;
 import android.annotation.NonNull;
 import android.annotation.Nullable;
@@ -353,9 +355,9 @@ public abstract class AbstractMultiProfilePagerAdapter extends PagerAdapter {
                 @UserIdInt int target) {
             IPackageManager packageManager = AppGlobals.getPackageManager();
 
-            return intents.stream().anyMatch(intent ->
-                    null != IntentForwarderActivity.canForward(intent, source, target,
-                            packageManager, intent.resolveTypeIfNeeded(mContentResolver)));
+            return intents.stream()
+                    .anyMatch(intent -> isCrossProfileIntent(
+                            intent, source, target, packageManager, mContentResolver));
         }
     }
 
--- java/src/com/android/intentresolver/ChooserActivity.java
+++ java/src/com/android/intentresolver/ChooserActivity.java
@@ -24,7 +24,7 @@ import static android.app.admin.DevicePolicyResources.Strings.Core.RESOLVER_CROS
 import static android.stats.devicepolicy.nano.DevicePolicyEnums.RESOLVER_EMPTY_STATE_NO_SHARING_TO_PERSONAL;
 import static android.stats.devicepolicy.nano.DevicePolicyEnums.RESOLVER_EMPTY_STATE_NO_SHARING_TO_WORK;
 
-import static com.android.intentresolver.util.IntentUtils.sanitizePayloadIntents;
+import static com.android.intentresolver.util.IntentUtils.prepareCrossProfileIntents;
 import static com.android.internal.util.LatencyTracker.ACTION_LOAD_SHARE_SHEET;
 
 import android.annotation.IntDef;
@@ -80,6 +80,7 @@ import androidx.viewpager.widget.ViewPager;
 
 import com.android.intentresolver.AbstractMultiProfilePagerAdapter.EmptyState;
 import com.android.intentresolver.AbstractMultiProfilePagerAdapter.EmptyStateProvider;
+import com.android.intentresolver.AbstractMultiProfilePagerAdapter.CrossProfileIntentsChecker;
 import com.android.intentresolver.NoCrossProfileEmptyStateProvider.DevicePolicyBlockerEmptyState;
 import com.android.intentresolver.chooser.DisplayResolveInfo;
 import com.android.intentresolver.chooser.MultiDisplayResolveInfo;
@@ -494,9 +495,17 @@ public class ChooserActivity extends ResolverActivity implements
                         /* devicePolicyEventId= */ RESOLVER_EMPTY_STATE_NO_SHARING_TO_WORK,
                         /* devicePolicyEventCategory= */ ResolverActivity.METRICS_CATEGORY_CHOOSER);
 
-        return new NoCrossProfileEmptyStateProvider(getPersonalProfileUserHandle(),
-                noWorkToPersonalEmptyState, noPersonalToWorkEmptyState,
-                createCrossProfileIntentsChecker(), createMyUserIdProvider());
+        return new NoCrossProfileEmptyStateProvider(
+                getPersonalProfileUserHandle(),
+                noWorkToPersonalEmptyState,
+                noPersonalToWorkEmptyState,
+                createCrossProfileIntentsChecker(),
+                createMyUserIdProvider());
+    }
+
+    @Override
+    protected CrossProfileIntentsChecker createCrossProfileIntentsChecker() {
+        return new AlwaysTrueCrossProfileIntentsChecker(getContentResolver());
     }
 
     private ChooserMultiProfilePagerAdapter createChooserMultiProfilePagerAdapterForOneProfile(
@@ -524,7 +533,21 @@ public class ChooserActivity extends ResolverActivity implements
             List<ResolveInfo> rList,
             boolean filterLastUsed) {
         int selectedProfile = findSelectedProfile();
-        List<Intent> crossProfileIntents = sanitizePayloadIntents(mIntents);
+        UserHandle personalProfileUserHandle = getPersonalProfileUserHandle();
+        UserHandle workProfileUserHandle = getWorkProfileUserHandle();
+        List<Intent> crossProfileIntents = selectedProfile == PROFILE_PERSONAL
+                ? prepareCrossProfileIntents(
+                        getContentResolver(),
+                        mChooserRequest.getTargetIntent(),
+                        mIntents,
+                        personalProfileUserHandle,
+                        workProfileUserHandle)
+                : prepareCrossProfileIntents(
+                        getContentResolver(),
+                        mChooserRequest.getTargetIntent(),
+                        mIntents,
+                        workProfileUserHandle,
+                        personalProfileUserHandle);
         ChooserGridAdapter personalAdapter = createChooserGridAdapter(
                 /* context */ this,
                 /* payloadIntents */ selectedProfile == PROFILE_PERSONAL
@@ -533,7 +556,7 @@ public class ChooserActivity extends ResolverActivity implements
                 selectedProfile == PROFILE_PERSONAL ? initialIntents : null,
                 rList,
                 filterLastUsed,
-                /* userHandle */ getPersonalProfileUserHandle());
+                /* userHandle */ personalProfileUserHandle);
         ChooserGridAdapter workAdapter = createChooserGridAdapter(
                 /* context */ this,
                 /* payloadIntents */ selectedProfile == PROFILE_WORK
@@ -542,15 +565,15 @@ public class ChooserActivity extends ResolverActivity implements
                 selectedProfile == PROFILE_WORK ? initialIntents : null,
                 rList,
                 filterLastUsed,
-                /* userHandle */ getWorkProfileUserHandle());
+                /* userHandle */ workProfileUserHandle);
         return new ChooserMultiProfilePagerAdapter(
                 /* context */ this,
                 personalAdapter,
                 workAdapter,
-                createEmptyStateProvider(/* workProfileUserHandle= */ getWorkProfileUserHandle()),
+                createEmptyStateProvider(/* workProfileUserHandle= */ workProfileUserHandle),
                 () -> mWorkProfileAvailability.isQuietModeEnabled(),
                 selectedProfile,
-                getWorkProfileUserHandle(),
+                workProfileUserHandle,
                 mMaxTargetsPerRow);
     }
 
